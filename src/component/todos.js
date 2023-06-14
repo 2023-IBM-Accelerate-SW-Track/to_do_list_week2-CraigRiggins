44feff0322cb82441e4a7703b145994bbb44020e
craig riggins <craigriggins@craigs-mbp.usca.ibm.com>
2023-06-14 16:41:16 -0700

    first commit week 2

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../component/todos.css";
-import { Card, CardContent, Grid } from "@mui/material";
+import { Card, CardContent, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -17,9 +17,10 @@ const Todos = ({ todos }) => {
           <Card>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
-            <CardContent>
-              <span style={{ padding: "50px" }}>{todo.content}</span>
-            </CardContent>
+            <Checkbox style={{paddingLeft:0}} color="primary"></Checkbox>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary={todo.content} seconday={todo.date} style={{marginTop:10}}/>
+            </ListItemButton>
           </Card>
         </Grid>
       );
